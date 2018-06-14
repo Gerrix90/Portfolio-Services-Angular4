fb8d2818d1f811e754422170b9084f8ff7245701
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2018-06-14 09:38:55 +0200

    Update unit test inject services

--- src/app/providers/auth.service.spec.ts
+++ src/app/providers/auth.service.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { AngularFireAuth } from 'angularfire2/auth';
 import { AuthService } from './auth.service';
+import * as firebase from 'firebase';
+
 
 describe('Auth.Service.TsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthService]
+      imports: [
+        firebase
+      ],
+      providers: [
+        AuthService,
+        AngularFireAuth,
+      ]
     });
   });
 
