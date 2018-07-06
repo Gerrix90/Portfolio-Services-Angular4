d578a8aad264013747c3edbe595abe27684a8925
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2018-07-06 22:41:24 +0200

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from './providers/auth.service';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Location } from '@angular/common';
+
 import * as firebase from 'firebase/app';
-import { NgClass } from '@angular/common';
-import { NgModel } from '@angular/forms';
+
 
 
 
@@ -21,17 +22,6 @@ import { NgModel } from '@angular/forms';
 
 export class AppComponent {
 
-  birthday = new Date(1985, 12, 16);
-
-  power = 5;
-
-  factor = 10;
-
-  toggle = true; // start with true == shortDate
-
-  get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
-  toggleFormat() { this.toggle = !this.toggle; }
-
   color: string;
 
   user: Observable<firebase.User>;
@@ -42,41 +32,42 @@ export class AppComponent {
 
   email: String;
 
-  constructor(public authService: AuthService, public afAuth: AngularFireAuth, private router: Router) {
+  constructor(
+    public authService: AuthService,
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    public _ActivatedRoute: ActivatedRoute,
+    public location : Location
+  ) {
 
-  this.user = this.authService.afAuth.authState;
+    this.user = this.authService.afAuth.authState;
 
-  this.user.subscribe( (auth) => {
+    this.user.subscribe((auth) => {
 
-    if (auth) {
+      if (auth) {
 
-      this.isLogged = true;
+        this.isLogged = true;
 
-      this.pseudo = auth.displayName;
+        this.pseudo = auth.displayName;
 
-      this.email = auth.email;
+        this.email = auth.email;
 
-      console.log('Connecté');
+        this.router.navigate(['login']);
 
-      console.log(auth);
 
-      this.router.navigate(['login']);
+      } else {
 
-    } else {
+        this.isLogged = false;
 
-      console.log('Déconnecté');
+        this.pseudo = '';
 
-      this.isLogged = false;
+        this.email = '';
 
-      this.pseudo = '';
+        this.router.navigate(['home']);
 
-      this.email = '';
+      }
 
-      this.router.navigate(['home']);
-
-    }
-
-  });
+    });
 
   }
 
