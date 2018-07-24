a67b7f2e0cc3f822aade25c5e387485d5e655b2e
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2018-07-24 23:11:39 +0200

    maj modals

--- src/app/modals/modals.component.ts
+++ src/app/modals/modals.component.ts
@@ -7,29 +7,8 @@ import { ModalService } from './modals.service';
     styleUrls: ['modals.component.css']
 })
 export class ModalComponent implements OnInit {
-  @Input() public modalId: string;
-  @Input() public modalTitle: string;
-  @Input() blocking = false;
-  isOpen = false;
-
-  @HostListener('keyup') onMouseEnter(event) {
-    this.keyup(event);
-  }
-
-  constructor(public modalService: ModalService) {
-  }
+  @Input() arrayModal;
 
   ngOnInit() {
-    this.modalService.registerModal(this);
-  }
-
-  close(checkBlocking = false): void {
-    this.modalService.close(this.modalId, checkBlocking);
-  }
-
-  public keyup(event: KeyboardEvent): void {
-    if (event.keyCode === 27) {
-      this.modalService.close(this.modalId, true);
-    }
   }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,15 @@
 import { Component, ApplicationRef, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from '../providers/auth.service';
+
+class NavModele {
+    url: string;
+    title: string;
+    constructor(url: string, title: string) {
+        this.url = url;
+        this.title = title;
+    }
+}
 
 @Component({
     moduleId: module.id,
@@ -9,17 +18,26 @@ import { AuthService } from '../providers/auth.service';
     styleUrls: ['nav.component.css']
 })
 
+
 export class NavComponent implements OnInit{
+
+    array = [];
+    sub: any;
+    constructor(public authService: AuthService, public router: Router, private route: ActivatedRoute) { }
     
-    constructor(public authService: AuthService, public router: Router) { }
-
     login() {
-
         this.authService.loginWithGoogle();
-        
     }
-        
+    
     ngOnInit() {
-        
+        this.array = [
+            /* new NavModele(null,'Services',this.login), */
+            new NavModele('#projects','Projects'),
+            new NavModele('#jobs','Job Expériences'),
+            new NavModele('#contact','Contact'),
+        ];
+        this.sub = this.route
+            .data
+            .subscribe(value => console.log(value));
     }
 }
