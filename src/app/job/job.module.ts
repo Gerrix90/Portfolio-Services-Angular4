0f855af0f856088ede8cdc04e1c2cd596b117370
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2018-07-24 23:11:01 +0200

    change style croos ico & deplace modal print to make only one component

--- src/app/job/job.module.ts
+++ src/app/job/job.module.ts
@@ -3,10 +3,11 @@ import { CommonModule, NgClass } from '@angular/common';
 import { JobComponent } from "./job.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JobService } from './job.service';
+import { ModalComponent } from '../modals/modals.component';
 
 @NgModule({
     imports: [ CommonModule,BrowserAnimationsModule ],
-    declarations: [ JobComponent ],
+    declarations: [ JobComponent, ModalComponent ],
     exports: [ JobComponent ],
     providers: [ JobService ]
 })
