12e1d4b4a5a2be9faccdcea5f8d1f19f1f878060
Adrien-Leteinturier <adrienleteinturier@yahoo.fr>
2018-10-11 20:44:54 +0200

    change postion ico close; refacto component

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -9,21 +9,21 @@ import {transition} from '@angular/animations';
     moduleId: module.id,
     animations: [
         trigger('displayElement',[
-
-        state('none',style({
-            'height': '60px'
-        })),
-        state('displayOk',style({
-            'height': '90%',
-            'background': 'linear-gradient(135deg, #ffffff 30%,#6639b6 100%)' /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-        })),
-    
-   
-        transition('none => displayOk',[
-            animate('1000ms cubic-bezier(0.6, -0.28, 0.735, 0.045)')
+            
+            state('none',style({
+                'height': '60px'
+            })),
+            state('displayOk',style({
+                'height': '90%',
+                'background': 'linear-gradient(135deg, #ffffff 30%,#6639b6 100%)' /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+            })),
+            
+            
+            transition('none => displayOk',[
+                animate('1000ms cubic-bezier(0.6, -0.28, 0.735, 0.045)')
             ]),
-        transition('displayOk => none',[
-            animate('500ms cubic-bezier(0.6, -0.28, 0.735, 0.045)')
+            transition('displayOk => none',[
+                animate('500ms cubic-bezier(0.6, -0.28, 0.735, 0.045)')
             ])
         ])       
     ],
@@ -32,51 +32,44 @@ import {transition} from '@angular/animations';
     styleUrls: ['Job.component.css']
 })
 
-    
+
 
 export class JobComponent{
     jobs: Job[];
     prints: Print[];
     
-
+    
     constructor(public _jobService: JobService){ 
     }
-
-
+    
+    
     ngOnInit() { // pour initialiser les données du servcice
-
-        this._jobService.getJobsFromAPIwithCache() 
-                         .subscribe( 
-                           res => this.jobs = res,
-                           err => console.error(err.status)
-                           ); 
-
-        this._jobService.getPrintsFromAPIwithCache() 
-                         .subscribe( 
-                           res => this.prints = res,
-                           err => console.error(err.status)
-                           ); 
-        }
-
-        
-        
-        
-        display(jobs: any){
-            jobs.stateAnim = 'displayOk';      
-        }    
-        
-        outDisplay(jobs: any){        
-            jobs.stateAnim = 'none';
-        }
-        
-        display2(prints: any){
-            prints.stateAnim = 'displayOk';        
-        }    
-
-        outDisplay2(prints: any){        
-            prints.stateAnim = 'none';
-        }
-        
+        this.callJobServiceFor(); 
+    }
+    
+    private callJobServiceFor() {
+        this._jobService.getJobsFromAPIwithCache()
+        .subscribe(res => this.jobs = res, err => console.error(err.status));
+        this._jobService.getPrintsFromAPIwithCache()
+        .subscribe(res => this.prints = res, err => console.error(err.status));
+    }
+    
+    display(jobs: any){
+        jobs.stateAnim = 'displayOk';      
+    }    
+    
+    outDisplay(jobs: any){        
+        jobs.stateAnim = 'none';
+    }
+    
+    display2(prints: any){
+        prints.stateAnim = 'displayOk';        
+    }    
+    
+    outDisplay2(prints: any){        
+        prints.stateAnim = 'none';
+    }
+    
 }
 
 
